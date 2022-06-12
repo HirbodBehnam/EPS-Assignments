67f213a1aaaf1c264dc9fe87256b1b10a0216b11
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2022-06-12 13:26:54 +0430

    Added stuff

--- 4/Practical/3.r
+++ 4/Practical/3.r
@@ -1,3 +1,6 @@
+library("ggplot2")
+library("reshape2")
+# Part 1
 simulator1 <- function(data) {
     return(sum((data - mean(data))^2) / (length(data)))
 }
@@ -8,5 +11,14 @@ true_variance <- 2^2
 data <- lapply(2:1000, function(i) rnorm(i, mean = 2, sd = sqrt(true_variance)))
 simulator1_bias <- sapply(data, function(i) simulator1(i) - true_variance)
 simulator2_bias <- sapply(data, function(i) simulator2(i) - true_variance)
-print(head(simulator1_bias))
-print(head(simulator2_bias))
+# Part 2
+bias_data <- data.frame(n = 2:1000, simulator1_data = simulator1_bias, simulator2_data = simulator2_bias)
+bias_data <- melt(bias_data, value.name = "bias", id = "n")
+ggplot(bias_data, aes(n, bias, colour = variable)) +
+  geom_point() + geom_smooth(colour = "black") + facet_wrap(vars(variable))
+# Part 3
+data <- lapply(1:10000, function(i) rnorm(10, mean = 2, sd = sqrt(true_variance)))
+simulator1_bias <- mean(sapply(data, function(i) simulator1(i) - true_variance))
+simulator2_bias <- mean(sapply(data, function(i) simulator2(i) - true_variance))
+cat("simulator1 bias is ", simulator1_bias, "\n")
+cat("simulator2 bias is ", simulator2_bias, "\n")
